a17b218a3aacc8f800a0932c04b05b67a96b24a1
Pratham Sikka <prathamsikka2004@gmail.com>
2024-01-31 17:39:39 +0530

    Add files via upload

--- url.js
+++ url.js
@@ -1,23 +1,30 @@
-const mongoose = require("mongoose");
+const shortid = require("shortid");
+const URL = require("../models/url");
 
-const urlSchema = new mongoose.Schema({
-    shortId: {
-        type: String,
-        required: true,
-        unique: true,
+async function handleGenerateNewShortURL(req, res) {
+  const body = req.body;
+  if (!body.url) return res.status(400).json({ error: "url is required" });
+  const shortID = shortid();
 
-    },
-    redirectURL:{
-type: String,
-required: true,
+  await URL.create({
+    shortId: shortID,
+    redirectURL: body.url,
+    visitHistory: [],
+  });
 
-    },
-    visitHistory: [ { timestamp: {type: Number}}],
-},
-{timestamps: true}
-);
+  return res.json({ id: shortID });
+}
 
-const URL = mongoose.model('url', urlSchema)
+async function handleGetAnalytics(req, res) {
+  const shortId = req.params.shortId;
+  const result = await URL.findOne({ shortId });
+  return res.json({
+    totalClicks: result.visitHistory.length,
+    analytics: result.visitHistory,
+  });
+}
 
-module.exports= URL;
-
+module.exports = {
+  handleGenerateNewShortURL,
+  handleGetAnalytics,
+};
